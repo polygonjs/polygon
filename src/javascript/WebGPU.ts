913a1c194deee79052e8bf0f46ad71c8bf9111ce
Guillaume Fradin <guillaumefradin@gmail.com>
2024-07-05 22:59:04 +0100

    shader tweak

--- src/javascript/WebGPU.ts
+++ src/javascript/WebGPU.ts
@@ -36,27 +36,27 @@ export async function setupAndRenderWebGPU() {
 		label: "our hardcoded red triangle shaders",
 		code: `
 
-		struct OurStruct {
-			offset: f32,
-		};
-	 
-		@group(0) @binding(0) var<uniform> ourStruct: OurStruct;
+struct OurStruct {
+	offset: f32,
+};
 
-		@vertex
-		fn vs( @builtin(vertex_index) vertexIndex : u32) -> @builtin(position) vec4f {
-			let pos = array(
-				vec2f( 0.0,  0.5+ourStruct.offset),  // top center
-				vec2f(-0.5, -0.5),  // bottom left
-				vec2f( 0.5, -0.5)   // bottom right
-			);
-		
-			return vec4f(pos[vertexIndex], 0.0, 1.0);
-		}
-		
-		@fragment
-		fn fs() -> @location(0) vec4f {
-			return vec4f(1.0, 0.0, 0.0, 1.0);
-		}
+@group(0) @binding(0) var<uniform> ourStruct: OurStruct;
+
+@vertex
+fn vs( @builtin(vertex_index) vertexIndex : u32) -> @builtin(position) vec4f {
+	let pos = array(
+		vec2f( 0.0,  0.5+ourStruct.offset),  // top center
+		vec2f(-0.5, -0.5),  // bottom left
+		vec2f( 0.5, -0.5)   // bottom right
+	);
+
+	return vec4f(pos[vertexIndex], 0.0, 1.0);
+}
+
+@fragment
+fn fs() -> @location(0) vec4f {
+	return vec4f(1.0, 0.0, 0.0, 1.0);
+}
 			`,
 	});
 
