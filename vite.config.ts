8d03581bfc16075eb2aab138b31179e41f7a13f6
Guillaume Fradin <guillaumefradin@gmail.com>
2024-08-22 22:37:38 +0100

    resize does not crash, but is probably not optimal

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { exec, ExecException, spawn, SpawnOptions } from "child_process";
 // import * as fs from "fs";
 // import * as path from "path";
 
-const BUILD_ON_FILE_CHANGE: boolean = true;
+const BUILD_ON_FILE_CHANGE: boolean = false;
 const RUN_NATIVE_ON_FILE_CHANGE: boolean = false;
 // const CMD_JAI = "jai";
 // const CMD_JAI_ARGS = ["src/jai/build.jai"];
